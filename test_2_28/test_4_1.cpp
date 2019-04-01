c3f08ce4f9d98a0b76a8a7cedb01347383472f4f
yhf869252662 <yhf753951@163.com>
2019-04-01 11:35:52 +0800

    test_4_1.cpp,变态跳台阶，快到碗里来

---
+++ test_2_28/test_4_1.cpp
@@ -0,0 +1,50 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//class Solution {
+//public:
+//	int jumpFloorII(int number) {
+//		if (number == 1)
+//			return 1;
+//		
+//		return 2 * jumpFloorII(number - 1);
+//	}
+//};
+//
+//int main()
+//{
+//	Solution so;
+//	int n = 0;
+//	
+//	cin >> n;
+//	cout << so.jumpFloorII(n) << endl;
+//
+//	return 0;
+//}
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//const double Pi = 3.1415926;
+//
+//int main()
+//{
+//	long double n = 0.0;
+//	long double r = 0.0;
+//
+//	while (cin >> n >> r)
+//	{
+//		if (r * 2 * Pi >= n)
+//		{
+//			cout << "Yes" << endl;
+//		}
+//		else
+//		{
+//			cout << "No" << endl;
+//		}
+//	}
+//
+//	return 0;
+//}
